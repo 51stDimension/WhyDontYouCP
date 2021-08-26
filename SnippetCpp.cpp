508676bfc1189469e694798d0e3be9804ad5e75e
51stDimension <83491618+51stDimension@users.noreply.github.com>
2021-08-26 17:55:15 +0530

    Update SnippetCpp.cpp

--- SnippetCpp.cpp
+++ SnippetCpp.cpp
@@ -1,5 +1,5 @@
 /**
-*Author:cmefly
+* @Author:51D
 **/
 #include <iostream>
 #include <string>
@@ -18,7 +18,26 @@
 #include <iterator>
 #include <algorithm>
 #include <sstream>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
 
+//Variant Technique Usage like 10101010 and 01010101
+//The value of str[i]^(i%2) is 1 for the first string and 0 for the second //string
+//Also sometimes try to think Binary
+//Sometimes think about array by denoting its elements in bar graph form
+//Break into smaller test cases such as n=2 n=3 and n=4
+//In transformation type questions focus on the endcase and also see if //its unique
+//First approach when n is small is do brute forces. Optimize after contest
+//set has duplicate elements issues(prefer multiset)
+//If multiple times sorting required use multiset
+//Sometimes think string traversal from back
+
+
+//See this during Debug Sessions
+//if(curr>=s.length() || curr<0 || visited[curr])
+//Always use the variable as a index after checking bounds
+
+using namespace __gnu_pbds;
 using namespace std;
 
 typedef long long ll;
@@ -30,10 +49,15 @@ typedef long long ll;
 #define w(t) ll tt;cin>>tt;while(tt--)
 #define pb push_back
 #define endl "\n"
-#define read(n) ll n;cin>>n;vector<int> arr(n);for(int i=0;i<n;i++){cin>>arr[i];};
-#define pi pair<int,int>
+#define read(arr,n) for(ll i=0;i<n;i++){cin>>arr[i];};
+#define pi pair<ll,ll>
+#define all(arr) arr.begin(),arr.end()
+
+template <typename T>
+using ordered_set = tree<T, null_type, greater_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
 
 const int N=2e5+5;
+const long long infll = 0x3f3f3f3f3f3f3f3fLL;
 int p[N];
 
 ll modulo(int a, int b, int n){
@@ -89,17 +113,73 @@ ll PrimeF(ll num){
 
     return count;
 }
-void solve(){
-    
-}   
+
+void addEdge(vector<ll> adj[],ll u,ll v){
+    adj[u].pb(v);
+    adj[v].pb(u);
+}
+
+ll __lcm(ll a,ll b){
+    return a*b/(__gcd(a,b));
+}
+
+vector<ll> fact;
+void factorialFill(){
+    fact.pb(1);
+    fact.pb(1);
+    for(ll i=2;i<=1000000;i++){
+        fact.pb((i%mod * fact.back()%mod)%mod);
+    }
+}
+ll getfact(ll num){
+    return fact[num]%mod;
+}
+
+struct cmp {
+    bool operator() (const pair<int, int> &a, const pair<int, int> &b) const {
+        return a.first < b.first;
+    }
+};
+//Use the above comparator when you want custom set sorting
+
+/*
+
+Start thinking,writing,just do something
+Testcases likhte reh figure out hojayega
+If its too tough kuch toh pattern hoga
+Ask questions to yourself
+Take baby steps with the question and then merge ur ideas
+Just Remember that the question has something hidden. (It is solvable)
+You dont want to regret after reading the editorial.
+Miro board mein shuroo hoja bas.
+During pair type questions:
+Maybe there is no need to check all pairs. Try proving that
+HashMap
+Seive
+All the best!
+
+
+Kisiko Ghanta Farak padta hai tere rating se.
+Solve kar. No matter how many times you have to submit. 
+If counfused with a conclusion try proving with n=2 and n=3
+
+Try Thinking Brute Force alongwith PigeonHole(Hard to observe)
+Whenever you get a constructive problem. Try solving for n=2,n=3,n=4 and then start generalizing till n=6
+*/
+void solve(ll tc){
+        
+}
 
 int main() {
 
     FIO;
     //seiveFill();
-    w(t)
-        solve();
+    //factorialFill();
+    ll tt;
+    cin>>tt;
+    for(ll i=1;i<=tt;i++){
+        solve(i);
+    }
 
-    
     return 0;
 }
