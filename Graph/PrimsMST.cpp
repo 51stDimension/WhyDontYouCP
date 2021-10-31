3a93726e4ab3eb8f0e06d4911710d161df1d7aa5
51stDimension <83491618+51stDimension@users.noreply.github.com>
2021-11-01 00:27:14 +0530

    Create PrimsMST.cpp

---
+++ Graph/PrimsMST.cpp
@@ -0,0 +1,53 @@
+const int INF = 1000000000;
+
+struct Edge {
+    int w = INF, to = -1;
+    bool operator<(Edge const& other) const {
+        return make_pair(w, to) < make_pair(other.w, other.to);
+    }
+};
+
+int n;
+vector<vector<Edge>> adj;
+
+void prim() {
+
+    int total_weight = 0;
+    vector<Edge> min_e(n);
+    // min_e[v] will store the weight of the smallest edge from vertex v to an already selected vertex 
+    // (again in the form of a weight and target pair)
+
+    min_e[0].w = 0;
+    //Not sure if this is required
+
+    set<Edge> q;
+
+    q.insert({0, 0});
+
+    vector<bool> selected(n, false);
+    for (int i = 0; i < n; ++i) {
+        if (q.empty()) {
+            cout << "No MST!" << endl;
+            return;
+        }
+
+        int v = (*q.begin()).to;
+        selected[v] = true;
+        total_weight += (*q.begin()).w;
+        q.erase(q.begin());
+
+        if (min_e[v].to != -1)
+            cout << v << " " << min_e[v].to << endl;
+
+        //Adding all the new edges
+        for (Edge e : adj[v]) {
+            if (!selected[e.to] && e.w < min_e[e.to].w) {
+                q.erase({min_e[e.to].w, e.to});
+                min_e[e.to] = {e.w, v};
+                q.insert({e.w, e.to});
+            }
+        }
+    }
+
+    cout << total_weight << endl;
+}
